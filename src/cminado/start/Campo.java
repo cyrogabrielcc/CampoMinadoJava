d1681e788deddda44f5ebac3f6fa8c47027fdf98
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-07-05 19:36:36 -0300

    Classe Campo

--- src/cminado/start/Campo.java
+++ src/cminado/start/Campo.java
@@ -1,12 +1,25 @@
 package cminado.start;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Campo {
 	public final int linha;
 	public final int coluna;
 	private boolean minado;
 
+	private boolean aberto=false;
+	private boolean fechado=false;
+	private boolean marcado=false;
+
+	private List<Campo> vizinhos = new ArrayList<>();
+
 	public Campo(int linha, int coluna) {
 		this.coluna=coluna;
 		this.linha=linha;
+	}
+
+	boolean adicionarVizinho(Campo Vizinho){
+		return false;
 	}
 }
