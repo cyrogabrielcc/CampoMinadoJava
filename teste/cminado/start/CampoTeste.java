8ef05ad4924f7f15f61e0ef05037b8475cfb7564
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-07-06 12:16:29 -0300

    Fechando teste Vizinhos

--- teste/cminado/start/CampoTeste.java
+++ teste/cminado/start/CampoTeste.java
@@ -1,5 +1,6 @@
 package cminado.start;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -16,11 +17,42 @@ public class CampoTeste {
     }
 
     @Test
-    public void testeVizinhoDistancia1(){
+    public void testeVizinhoDistanciaDireita(){
+        Campo vizinho = new Campo(3,4);
+        boolean resultado = campo.adicionarVizinho(vizinho);
+        assertTrue(resultado);
+    }
+
+    @Test
+    public void testeVizinhoDistanciaEsquerda(){
         Campo vizinho = new Campo(3,2);
         boolean resultado = campo.adicionarVizinho(vizinho);
         assertTrue(resultado);
     }
-    
+    @Test
+    public void testeVizinhoDistanciaCima(){
+        Campo vizinho = new Campo(2,3);
+        boolean resultado = campo.adicionarVizinho(vizinho);
+        assertTrue(resultado);
+    }
+    @Test
+    public void testeVizinhoDistanciaBaixo(){
+        Campo vizinho = new Campo(4,3);
+        boolean resultado = campo.adicionarVizinho(vizinho);
+        assertTrue(resultado);
+    }
 
+    @Test
+    public void testeVizinhoDistanciaDiagonal(){
+        Campo vizinho = new Campo(2,2);
+        boolean resultado = campo.adicionarVizinho(vizinho);
+        assertTrue(resultado);
+    }
+
+    @Test
+    public void testeNaoVizinhoDistancia(){
+        Campo vizinho = new Campo(1,1);
+        boolean resultado = campo.adicionarVizinho(vizinho);
+        assertFalse(resultado);
+    }
 }
