df0693dba790fe58cbe13d1f832b3cb1d23abdc5
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-07-06 12:08:38 -0300

    test

--- teste/cminado/start/CampoTeste.java
+++ teste/cminado/start/CampoTeste.java
@@ -3,10 +3,17 @@ package cminado.start;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 public class CampoTeste {
     
     private Campo campo = new Campo(3,3);
+
+    @BeforeEach
+    public void iniciarCampo(){
+        campo = new Campo(3, 3);
+        
+    }
 
     @Test
     public void testeVizinhoDistancia1(){
