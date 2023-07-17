d46733a038ae8b290e0b516ba9d4603e75b98502
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-07-17 10:32:55 -0300

    iniciando tabuleiro

---
+++ src/cminado/start/Tabuleiro.java
@@ -0,0 +1,15 @@
+package cminado.start;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tabuleiro {
+    private int linhas;
+    private int colunas;
+    private int minas;
+    private final List<Campo> campos = new ArrayList<>();
+
+    public Tabuleiro() {
+        
+    }
+}
