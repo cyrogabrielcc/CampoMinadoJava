f086ef2ba44715e0911400d5246a1d39e3337460
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-07-17 11:18:10 -0300

    237 Classe tabuleiro

--- src/cminado/start/Tabuleiro.java
+++ src/cminado/start/Tabuleiro.java
@@ -8,8 +8,35 @@ public class Tabuleiro {
     private int colunas;
     private int minas;
     private final List<Campo> campos = new ArrayList<>();
+    
+    
+    public Tabuleiro(int linhas, int colunas, int minas) {
+        this.linhas = linhas;
+        this.colunas = colunas;
+        this.minas = minas;
 
-    public Tabuleiro() {
-        
+        gerarCampos();
+        associarVizinhos();
+        sortearMinas();
+    }
+
+    private void gerarCampos() {
+        for (int linha = 0; linha < linhas; linha++) {
+            for (int coluna = 0; coluna < colunas; coluna++) {
+                campos.add(new Campo(linha, coluna));
+            }
+        }
+    }
+
+    private void associarVizinhos() {
+        for (Campo c1 : campos) {
+            for (Campo c2 : campos) {
+                c1.adicionarVizinho(c2);
+            }  
+        }
+    }
+
+
+    private void sortearMinas() {
     }
 }
