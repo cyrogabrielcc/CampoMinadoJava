f7201580e3e21306e2a707e2251e441e4b444957
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-07-19 08:00:06 -0300

    subindo

--- src/cminado/start/Aplicacao.java
+++ src/cminado/start/Aplicacao.java
@@ -4,6 +4,11 @@ public class Aplicacao {
 
 	public static void main(String[] args) {
         Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
+
+		tabuleiro.abrir(3, 3);
+		tabuleiro.alterarMarcacao(4,4);
+		tabuleiro.alterarMarcacao(4,5);
+	
 	}
 
 }
